4173e43beb15d5f5b439fd7ceff09b7ac0100828
zah-femboy <Emmanuelkline22@outlook.com>
2024-06-28 16:41:35 -0700

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -2282,7 +2282,7 @@ do
         Position = UDim2.fromOffset(5, 2),
         TextXAlignment = Enum.TextXAlignment.Center,
         
-        Text = '[ Keybinds ]';
+        Text = ' Keybinds ';
         ZIndex = 104;
         Parent = KeybindInner;
     });
